78441b6cf0b51cff631aa6706bea3f8ffa540973
Giancarlo Horta <giancarlohorta@gmail.com>
2020-05-20 16:54:29 -0300

    add js modal

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("oi");
+const buttonModal = document.querySelector('[data-modal="button-enter"]');
+const modal = document.querySelector('[data-modal="container"]');
+const buttonClose = document.querySelector('[data-modal="fechar"]');
+
+console.log(buttonModal);
+
+function openModal() {
+  modal.classList.add("active");
+}
+function closeModal() {
+  modal.classList.remove("active");
+}
+buttonModal.addEventListener("click", openModal);
+buttonClose.addEventListener("click", closeModal);
